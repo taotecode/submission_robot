ca9e9299311770ae26962fbb590a605d0be04c81
taotecode <48904825+taotecode@users.noreply.github.com>
2023-12-10 16:44:37 +0800

    优化seeder

--- app/Admin/Controllers/ManuscriptController.php
+++ app/Admin/Controllers/ManuscriptController.php
@@ -57,7 +57,7 @@ class ManuscriptController extends AdminController
                 return $html.'</div>';
             });
             $grid->column('one_approved')->display(function ($one_approved) {
-                return count($one_approved);
+                return get_posted_by($one_approved);
             })->expand(function () {
                 // 返回显示的详情
                 $uid = $this->one_approved['id'] ?? '';
@@ -68,7 +68,7 @@ class ManuscriptController extends AdminController
                 return "<div style='padding:10px 10px'><p>UID: $uid</p><p>first name: $first_name</p><p>last name: $last_name</p><p>用户名: $username</p></div>";
             });
             $grid->column('one_reject')->display(function ($one_reject) {
-                return count($one_reject);
+                return get_posted_by($one_reject);
             })->expand(function () {
                 // 返回显示的详情
                 $uid = $this->one_reject['id'] ?? '';
