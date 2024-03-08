ab48fa607ddc38800c4a0f496ceef2a5d780248c
taotecode <48904825+taotecode@users.noreply.github.com>
2024-03-09 00:04:00 +0800

    修复BUG

--- app/Admin/Forms/MyBot/IsAutoKeyword.php
+++ app/Admin/Forms/MyBot/IsAutoKeyword.php
@@ -44,22 +44,23 @@ class IsAutoKeyword extends Form implements LazyRenderable
 
         $this->radio('is_auto_keyword')
             ->when('1', function (Form $form) use ($id, $keyword, $lexicon) {
-                $form->textarea('keyword')->default('新闻')->value($keyword)->help('每行一个关键词<br>当稿件文本经过词库分词后的词语中，含有关键词，则会在消息尾部加入如：#新闻 #的标签');
-                $form->textarea('lexicon')->default('新闻')->value($lexicon)->help('
+                $form->textarea('keyword')->help('每行一个关键词<br>当稿件文本经过词库分词后的词语中，含有关键词，则会在消息尾部加入如：#新闻 #的标签')->default('新闻')->value($keyword);
+                $form->textarea('lexicon')->help('
                     词库格式为每行一个词，如果需要提升分词准确率，可以在词语后面加上词性，词性之间用空格隔开，词性列表如下：<br>
         新闻 1<br>
         一般数值在1-10之间，数值越大，分词越准确，但是分词速度越慢，建议平均值为：3<br>
         可以点击<a href="'.route('dcat.admin.bots.lexiconCheck', $id).'">词库验证</a>进行分词测试<br>
-                    ');
+                    ')->default('新闻')->value($lexicon);
             })
             ->options([
                 '0' => '关闭自动关键词',
                 '1' => '开启自动关键词',
             ])
-            ->default('1')->value($is_auto_keyword)->help('
+            ->help('
 开启自动关键词后，会自动在消息尾部加入关键词标签，如：#新闻 #的标签<br>
 注意：开启自动关键词后，需要在下方填写关键词和词库，否则无法正常工作。<br>
-注意：服务器性能最少需要1核2G内存，否则会导致分词失败。对服务器性能要求较高。');
+注意：服务器性能最少需要1核2G内存，否则会导致分词失败。对服务器性能要求较高。')
+            ->default('1')->value($is_auto_keyword);
 
         $this->hidden('id')->default($id);
     }
