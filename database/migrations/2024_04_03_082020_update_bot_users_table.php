ebf234ef3903e99bf437c7dfdfda526e19034f73
taotecode <48904825+taotecode@users.noreply.github.com>
2024-04-03 16:50:57 +0800

    开发使用机器人用户功能

---
+++ database/migrations/2024_04_03_082020_update_bot_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('bot_users')) {
+            Schema::table('bot_users', function (Blueprint $table) {
+                $table->string('user_id',100)->nullable()->comment('用户id')->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submission_user');
+    }
+};
