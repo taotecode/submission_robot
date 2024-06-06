fb288e63bba90d3879ab7deea5c2455bd9a9e333
taotecode <48904825+taotecode@users.noreply.github.com>
2024-06-06 23:08:55 +0800

    修复list和who指令的bug和权限

--- app/Helpers/message.php
+++ app/Helpers/message.php
@@ -7,7 +7,7 @@ function getCacheMessageData($objectType, $chatId, $tag): array
 {
     $messageCache = Cache::tags($tag.'.'.$chatId)->get($objectType);
     $messageId = $messageCache['message_id'] ?? '';
-    $messageText = $messageCache[$objectType]['text'] ?? $messageCache['caption'] ?? '';
+    $messageText = $messageCache['text'] ?? $messageCache['caption'] ?? '';
 
     return [$messageCache, $messageId, $messageText];
 }
