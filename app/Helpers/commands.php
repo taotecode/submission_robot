f86836ce71ab6bec8bad9f45944354d5abeed351
taotecode <48904825+taotecode@users.noreply.github.com>
2025-05-08 21:10:22 +0800

    在获取命令时，优化数据处理，确保命令数据被正确解码为数组格式。

--- app/Helpers/commands.php
+++ app/Helpers/commands.php
@@ -6,6 +6,7 @@ function get_command($botInfo,$key)
     $commands = $botInfo->bot_command;
     foreach ($commands as $command){
         if ($command->command===$key){
+            $command->data = json_decode($command->data, true);
             return $command;
         }
     }
